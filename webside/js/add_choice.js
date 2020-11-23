ff913d4da838d687a60b861e50cdf32225a4c583
Travis <travisthomps0n@hotmail.com>
2020-11-23 00:18:21 -0500

    added admin panel mockup and finalized the add choice code(needs to be tested)

--- webside/js/add_choice.js
+++ webside/js/add_choice.js
@@ -1,10 +1,13 @@
 function onAddChoice(e) {
     let data = {};
-    data["description"] = "";
-    data["teamMembers"] = 3;
+    data["description"] = document.getElementById("choiceDescription").value;
+    data["teamMembers"] = parseInt(document.getElementById("choiceTeamMembers").value);
+    if(isNaN(data["teamMembers"])) {
+        data["teamMembers"] = 0;
+    }
     
-    for(let i = 0; i < 5; i++) {
-        let alternative = "";
+    for(let i = 1; i <= 5; i++) {
+        let alternative = document.getElementById("choiceAlt" + i).value;
         
         if(!!alternative) {
             data["alternative" + i] = alternative;
