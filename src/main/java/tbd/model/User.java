021367394cffebe109236b3f5783fe10f5df2489
Andy <mikejeff5353@gmail.com>
2020-12-01 11:52:48 -0500

    created like choice js

--- src/main/java/tbd/model/User.java
+++ src/main/java/tbd/model/User.java
@@ -3,20 +3,20 @@ package tbd.model;
 public class User {
 
 	String uuid;
-	String choiceUUID;
+	String choiceID;
 	String name;
 	String password;
 	
 	public User(String u, String c, String n) {
 		this.uuid = u;
-		this.choiceUUID = c;
+		this.choiceID = c;
 		this.name = n;
 		this.password = null;
 	}
 
 	public User(String u, String c, String n, String p) {
 		this.uuid = u;
-		this.choiceUUID = c;
+		this.choiceID = c;
 		this.name = n;
 		this.password = p;
 	}
