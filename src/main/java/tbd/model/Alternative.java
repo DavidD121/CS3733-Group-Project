0435dde87c423694aea666e2b31bbf6acd8a1fd0
Robert Brodin <43351978+robertbrodin@users.noreply.github.com>
2020-12-05 15:01:32 -0500

    Updated Alternative with likes and dislikes

--- src/main/java/tbd/model/Alternative.java
+++ src/main/java/tbd/model/Alternative.java
@@ -6,22 +6,35 @@ public class Alternative {
 
 	public final String name;
 	public final String choiceID;
+	public int alternativeID;
 	public int likes;
 	public int dislikes;
-	public ArrayList<Feedback> listOfFeedback;
+	public String listOfFeedback;
+	public ArrayList<String> likedUsers;
+	public ArrayList<String> dislikedUsers;
 
 	
-	public Alternative(String name, String choiceID) {
+	public Alternative(String name, String choiceID, int alternativeID) {
 		this.name = name;
 		this.choiceID = choiceID;
+		this.alternativeID = alternativeID;
 	};
+	
+	
 
-	public Alternative(String name, String choiceID, int likes, int dislikes, ArrayList<Feedback> listOfFeedback) {
+	public Alternative(String name, String choiceID, int likes, int dislikes, ArrayList<String> likedUsers, ArrayList<String> dislikedUsers, String listOFFeedback) {
 		this.name = name;
 		this.choiceID = choiceID;
 		this.likes = likes;
 		this.dislikes = dislikes;
+		this.likedUsers = likedUsers;
+		this.dislikedUsers = dislikedUsers;
 		this.listOfFeedback = listOfFeedback;
+	}
+	
+	
+	public String toStringJSON() {
+		return "{\"name\":" + name + ", \"likes\":" + likes + ", \"dislikes\":" + dislikes + ", \"feedback\": \"\" }";
 	}
 	
 }
